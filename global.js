7e3431689d9b82859cd5f3f1398bafb9e618d2d6
Madhan P <madhansai98@gmail.com>
2018-08-21 22:33:27 +0530

    global objects learnt

---
+++ global.js
@@ -0,0 +1,25 @@
+function printHello() {
+    var a = "Printing data ...";
+    console.time(a);
+    console.log("Hello world");
+    console.error("Repeated");
+    console.timeEnd(a);
+}
+console.log(__filename);
+console.log(__dirname);
+console.log(process.execPath)
+console.log(process.arch + " " + process.platform);
+//calls function after 2 secs only once
+setTimeout(printHello, 2000);
+//calls functions every 2 secs
+//setInterval(printHello, 2000);
+//No. of seconds node is running
+var secs = process.uptime();
+console.log(secs);
+//memory usage by node
+var mem = process.memoryUsage();
+console.log(mem.s);
+
+process.on("exit", function() {
+    console.log("Exiting...");
+});
