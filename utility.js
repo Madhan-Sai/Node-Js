ac79df24c7180320e0e41bb67ab5d409dea69eee
Madhan P <madhansai98@gmail.com>
2018-08-21 23:47:40 +0530

    utilities modified

--- utility.js
+++ utility.js
@@ -5,4 +5,5 @@ console.log("Host: " + os.hostname());
 console.log("Type: " + os.type());
 console.log("Arch: " + os.arch());
 console.log("Uptime in minutes: " + os.uptime() / 60)
-console.log(os.networkInterfaces());
+console.log("Free memory: " + os.freemem() / (1024 * 1024 * 1024));
+console.log("Total memory: " + os.totalmem() / (1024 * 1024 * 1024));
